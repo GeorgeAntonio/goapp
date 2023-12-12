9c4cdd57af124351419c071d15e8bce85555164c
George Antonio <george.antonio13@hotmail.com>
2023-12-12 18:30:15 -0300

    initial

---
+++ src/actions.ts
@@ -0,0 +1,32 @@
+import { State } from './state';
+
+export interface Action {
+  name: string;
+  precond: State;
+  effects: State;
+  cost: number;
+}
+
+// Exemplos de ações para o berserker
+const searchEnemy: Action = {
+  name: 'searchEnemy',
+  precond: { beingAttacked: false, enemyKilled: false, hasEnemy: false },
+  effects: { beingAttacked: false, enemyKilled: false, hasEnemy: true },
+  cost: 1,
+};
+
+const attack: Action = {
+  name: 'attack',
+  precond: { beingAttacked: false, hasEnemy: true, enemyKilled: false },
+  effects: { beingAttacked: false, enemyKilled: true, hasEnemy: true },
+  cost: 2,
+};
+
+const defend: Action = {
+  name: 'defend',
+  precond: { beingAttacked: true, hasEnemy: true, enemyKilled: false },
+  effects: { beingAttacked: false, enemyKilled: false, hasEnemy: true },
+  cost: 1,
+};
+
+export const actions = [searchEnemy, attack, defend];
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { actions } from './actions';
+import { Npc } from './npc';
+import { State } from './state';
+
+// Definindo o estado inicial do berserker
+const state: State = {
+  beingAttacked: false,
+  hasEnemy: true,
+  enemyKilled: false,
+  // Adicione mais propriedades conforme necessário
+};
+
+// Definindo o objetivo do berserker
+const goal: State = {
+  beingAttacked: false, // Valor irrelevante para o objetivo
+  hasEnemy: false,      // Valor irrelevante para o objetivo
+  enemyKilled: true,    // Objetivo principal
+};
+
+
+// Criando a instância do berserker
+const berserker = new Npc(state, actions);
+berserker.goal = goal;
+
+// Exibindo o estado inicial
+console.log(berserker.state);
+
+// Executando o plano
+while (berserker.plan.length > 0) {
+  berserker.execute();
+}
+
+// Exibindo o estado final após a execução do plano
+console.log(berserker.state);
